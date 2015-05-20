ad08a7c62d2be24f8a93ced0895e50db1472eff6
Engin Eren <eren@nafhh-cms05.desy.de>
2015-05-20 16:53:48 +0200

    Added files'path

--- userlist.sh
+++ userlist.sh
@@ -4,11 +4,14 @@ list=$(grep "/user/" $1)
 touch bad.txt
 rm user.files.txt
 for user in $list; do
+  echo $user > tmp.txt
   s1=${user%'"'>*} 
   s1=${s1##*/user/}
   username=${s1%%/*}  
-  filename=${s1##*/}
+  filename=$(awk -F'"' '{print $2}' tmp.txt) 
+  #filename=${s1#*/}
   echo $username $filename >> bad.txt
+  rm tmp.txt
 done
 
 awk '!/entry/' bad.txt > temp && mv temp user.files.txt
