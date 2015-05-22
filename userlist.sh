49b63b95d5f24a336e22757213dd6719fe1332f5
Engin Eren <eren@nafhh-cms05.desy.de>
2015-05-22 13:23:05 +0200

    Now it creates sorted files

--- userlist.sh
+++ userlist.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-cat $1 | awk -F/ '{print $8}' > user
-cat $1 | awk -F'"' '{print $2}' > filename
+rm user.files.txt
 
-paste user filename | awk '{print $1,$2}' > user.files.txt
+cat $1 | grep /user/ | awk -F/ '{print $8}' > user
+cat $1 | grep /user/ | awk -F'"' '{print $2}' > filename
+
+paste user filename | awk '{print $1,$2}' | sort -n > user.files.txt
 
 rm user
 rm filename
