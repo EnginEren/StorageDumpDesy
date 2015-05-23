dea1bd373821741232597c180f32ca9c9a6ab0f3
Engin Eren <eren@nafhh-cms05.desy.de>
2015-05-23 14:53:24 +0200

    simple script to generate a html file

---
+++ generate_html.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+IFS=$'\n'
+set -f
+
+comm -3 $1 $2 | awk '{print $1,$2}' > trash
+
+cat << _EOF_
+<!DOCTYPE html>
+<html>
+<body>
+<table border="1" cellpadding="5" cellspacing="5">
+ <caption>Users who did not access files in tier2/store/user directory over six months </caption>
+_EOF_
+
+
+for i in $(cat trash); do
+	username=$(echo $i | awk '{print $1}')
+	filename=$(echo $i | awk '{print $2}')
+	 
+cat << _EOF_
+<tr>
+   <td>$username</td>
+   <td>$filename</td>
+</tr>
+_EOF_
+done
+
+cat << _EOF_
+</table>
+</body>
+</html>
+_EOF_
+
+
+exit 0;
+
+
+
+
+
+
+ 
+#        cat << _EOF_
+#	<!DOCTYPE html>
+#	<html>
+#	<body>
+
+#	<table border="1" style="width:100%">
+ # 	<tr>
+ #          <td>$username</td>
+ #   	   <td>$file</td>
+  #	</tr>
+  #	</table>
+
+#	</body>
+#	</html>
+
+#_EOF_
+
+#done
+
+#exit 0; 
