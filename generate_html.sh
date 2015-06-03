745af2fa29e934e851a198eb55b523859e360257
Engin Eren <eren@nafhh-cms05.desy.de>
2015-06-03 14:06:07 +0200

    few changes

--- generate_html.sh
+++ generate_html.sh
@@ -3,9 +3,9 @@
 IFS=$'\n'
 set -f
 
-#comm -3 $1 $2 | awk '{print $1,$2}' > trash
+comm -12 $1 $2 | awk '{print $1,$2}' > notouch
 
-cat new | awk '{print $1}' | sort -u > onlyname
+cat notouch | awk '{print $1}' | sort -u > onlyname
 
 touch users.html
 
@@ -18,7 +18,7 @@ Users@T2_DESY
 </title>
 </head>
 <body>
-<h2> Users who did not access/touch files in tier2/store/user directory over six months </h2>
+<h2> Users who did not access/touch files in tier2/store/user directory between 01.03.2015-31.05.2015 </h2>
 <img src="user-group-icon.png" alt="People" style="width:256px"> " > users.html
 for i in $(cat onlyname); do
 	touch user_$i.html
@@ -26,12 +26,12 @@ for i in $(cat onlyname); do
 <html>
 <body>
 <BODY BACKGROUND="speckle.gif">
-<h2>You did not access these files in tier2/store/user directory over six months </h2> " > user_$i.html
+<h2>Dear $i, you did not access these files in tier2/store/user directory between 01.03.2015-31.05.2015 </h2> " > user_$i.html
 	user="$i"
 	echo "<ul>
 		<li> <p><a href="https://eeren.web.cern.ch/eeren/user_$i.html"> $i</a></p> </li> 
 	      </ul>" >> users.html
-	for j in $(cat new); do
+	for j in $(cat notouch); do
 		username=$(echo $j | awk '{print $1}')
 		filename=$(echo $j | awk '{print $2}')
 		if [ "$username" == "$user" ]; then
@@ -48,7 +48,6 @@ done
 
 echo "</body>
 </html>" >> users.html
-
 
 
 exit 0;
