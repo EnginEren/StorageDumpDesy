b09fcf535ff2efac461babfb9ed93f3d1f04d9b8
Engin Eren <eren@nafhh-cms05.desy.de>
2015-06-04 09:48:29 +0200

    uncessary for loop has been removed

--- generate_html.sh
+++ generate_html.sh
@@ -19,7 +19,7 @@ Users@T2_DESY
 </head>
 <body>
 <h2> Users who did not access/touch files in tier2/store/user directory between 01.03.2015-31.05.2015 </h2>
-<img src="user-group-icon.png" alt="People" style="width:256px"> " > users.html
+<img src="user-group-icon.png" alt="People" style="width:256px" align="right"> " > users.html
 for i in $(cat onlyname); do
 	touch user_$i.html
 	echo "<!DOCTYPE html>
@@ -31,15 +31,9 @@ for i in $(cat onlyname); do
 	echo "<ul>
 		<li> <p><a href="https://eeren.web.cern.ch/eeren/user_$i.html"> $i</a></p> </li> 
 	      </ul>" >> users.html
-	for j in $(cat notouch); do
-		username=$(echo $j | awk '{print $1}')
-		filename=$(echo $j | awk '{print $2}')
-		if [ "$username" == "$user" ]; then
-		echo "<ul>
-    			<li> $filename </li>
-    		      </ul>" >> user_$i.html
-		fi 	
-	done		
+        echo "<pre>" >> user_$i.html
+	grep $i notouch | awk '{print $2}' >> user_$i.html
+	echo "</pre>" >> user_$i.html
 echo "</body>
 </html>" >> user_$i.html
 
